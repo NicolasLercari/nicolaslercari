95e782f96174282ade054ca851aaea71c996e6ff
NicolasLercari <nicolaslercari@gmail.com>
2024-10-25 19:26:59 +0200

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 export default defineConfig({
+    base: './',
     plugins: [react()],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src')
         }
+    },
+    build: {
+        outDir: 'dist',
+        rollupOptions: {
+            input: path.resolve(__dirname, 'index.html'), // Ensure this path is correct
+        },
     },
 })
